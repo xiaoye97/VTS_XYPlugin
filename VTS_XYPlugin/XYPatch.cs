5a8191a238ba7308726813d6a1bc48fa67a5be6b
宵夜 <34893504+xiaoye97@users.noreply.github.com>
2022-06-18 20:26:02 +0800

    b4

--- VTS_XYPlugin/XYPatch.cs
+++ VTS_XYPlugin/XYPatch.cs
@@ -5,10 +5,17 @@ namespace VTS_XYPlugin
     public static class XYPatch
     {
         [HarmonyPostfix, HarmonyPatch(typeof(SceneItem), "Initialize")]
-        public static void SceneItemPatch(SceneItem __instance)
+        public static void SceneItemInitializePatch(SceneItem __instance)
         {
             XYLog.LogMessage($"初始化了场景物品:{__instance.ItemInfo.ItemName}");
             __instance.gameObject.AddComponent<SceneItemBehaviour>();
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(SceneItem), "ToggleLock")]
+        public static void SceneItemToggleLockPatch(SceneItem __instance)
+        {
+            // 用于防止礼物碰撞体在锁定的状态下也移动
+            __instance.GestureTransformer.enabled = !__instance.ItemInfo.IsLocked;
         }
 
         [HarmonyPostfix, HarmonyPatch(typeof(HotkeyConfigItem), "ModelConfigChanged")]
